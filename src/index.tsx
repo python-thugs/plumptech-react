bd819e138cb301580506714bc44f29b031b01c4c
plump_albert <plumpalbert@gmail.com>
2022-06-15 13:11:21 +0300

    setup MUI and TailwindCSS

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {StyledEngineProvider} from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+      <CssBaseline />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: "div#root",
+  corePlugins: {
+    preflight: false,
+  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
