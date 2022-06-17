f8c925250bf8c74a5db6c90e8bb3396008f963b0
plump_albert <plumpalbert@gmail.com>
2022-06-17 13:06:57 +0300

    navigate user to login page if no auth data in redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import {useMemo} from "react";
-import {Routes, Route} from "react-router-dom";
+import {useEffect, useMemo} from "react";
+import {Routes, Route, useNavigate} from "react-router-dom";
 import LoginPage from "./views/default/pages/LoginPage";
 import AdminView from "./views/admin";
 import {useAppSelector} from "./store";
@@ -8,6 +8,11 @@ import AppBar from "./components/AppBar";
 
 function App() {
   const user = useAppSelector(store => store.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user.post) navigate("/login", {replace: true});
+  }, []); // eslint-disable-line
 
   const appView = useMemo(() => {
     switch (user.post?.id) {
