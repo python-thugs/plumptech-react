1b3a316da1630ab7986cea7f2b6f854818154cbe
plump_albert <plumpalbert@gmail.com>
2022-06-17 17:26:51 +0300

    set body default type as any

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 export const API_ENDPOINT =
   process.env.REACT_APP_API_ENDPOINT || "http://localhost:5000";
 
-export type Error<T> = {
+export type Error<T = any> = {
   /** Shows that response is error */
   error: true;
   /** Optional error message */
@@ -17,6 +17,6 @@ export type Error<T> = {
  * @param {any} [body] - additional error information
  * @returns {Error<any>}
  */
-export function error(message = "", body = undefined) {
+export function error(message = "", body: any = undefined) {
   return {error: true, message, body};
 }
