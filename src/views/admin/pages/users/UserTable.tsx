423bc824173aa4c57a78dcf742b70d156566f1e9
plump_albert <plumpalbert@gmail.com>
2022-06-18 09:52:18 +0300

    handling delete user button click

--- src/views/admin/pages/users/UserTable.tsx
+++ src/views/admin/pages/users/UserTable.tsx
@@ -1,3 +1,4 @@
+import {useCallback, useMemo, useState} from "react";
 import IconButton from "@mui/material/IconButton";
 import Table from "@mui/material/Table";
 import TableHead from "@mui/material/TableHead";
@@ -10,41 +11,94 @@ import PrintIcon from "@mui/icons-material/Print";
 import ResetIcon from "@mui/icons-material/LockReset";
 // custom imports
 import {IEmployee, PostEnum} from "../../../../api/types";
-import {useMemo} from "react";
+import {deleteUser} from "../../../../api/users";
+
+interface IError {
+  type: "info" | "error";
+  action: string;
+  message: string;
+}
+interface IUserRowProps extends IEmployee {
+  showFeedback: (msg: IError) => void;
+}
+
+const UserRow: React.FC<IUserRowProps> = ({
+  id,
+  name,
+  username,
+  post,
+  showFeedback,
+}) => {
+  const handlePrintClick = useCallback(() => {
+    alert("There should be a print dialog");
+  }, [id]);
+
+  const handleDeleteClick = useCallback(() => {
+    deleteUser(id)
+      .then(success => {
+        if (!success) {
+          showFeedback({
+            type: "error",
+            action: "delete-user",
+            message: `Возникла ошибка при удалении пользователя`,
+          });
+          return;
+        }
+        showFeedback({
+          type: "info",
+          action: "delete-user",
+          message: `Пользователь ${name} успешно удален`,
+        });
+      })
+      .catch(err => {
+        showFeedback({
+          type: "error",
+          action: "delete-user",
+          message: err.response ? err.response.data.message : err.message,
+        });
+      });
+  }, [id, name]);
+
+  return (
+    <TableRow
+      checkbox={post.id !== PostEnum.Администратор}
+      name={name}
+      userName={username}
+      post={post.name}
+      actions={
+        <>
+          <IconButton color="inherit" onClick={handlePrintClick}>
+            <PrintIcon />
+          </IconButton>
+          {post.id !== PostEnum.Администратор && (
+            <IconButton color="inherit">
+              <EditIcon />
+            </IconButton>
+          )}
+          <IconButton color="inherit">
+            <ResetIcon />
+          </IconButton>
+          {post.id !== PostEnum.Администратор && (
+            <IconButton color="inherit" onClick={handleDeleteClick}>
+              <DeleteIcon />
+            </IconButton>
+          )}
+        </>
+      }
+    />
+  );
+};
 
 const UserTable: React.FC<{users: IEmployee[]}> = ({users}) => {
   const employees = useMemo(
-    () =>
-      users.map(({name, username, post}) => (
-        <TableRow
-          checkbox={post.id !== PostEnum.Администратор}
-          name={name}
-          userName={username}
-          post={post.name}
-          actions={
-            <>
-              <IconButton color="inherit">
-                <PrintIcon />
-              </IconButton>
-              {post.id !== PostEnum.Администратор && (
-                <IconButton color="inherit">
-                  <EditIcon />
-                </IconButton>
-              )}
-              <IconButton color="inherit">
-                <ResetIcon />
-              </IconButton>
-              {post.id !== PostEnum.Администратор && (
-                <IconButton color="inherit">
-                  <DeleteIcon />
-                </IconButton>
-              )}
-            </>
-          }
-        />
-      )),
+    () => users.map(user => <UserRow {...user} />),
     [users]
   );
+
+  const [showAddDialog, setShowAddDialog] = useState(false);
+  const handleAddDialogToggle = useCallback(() => {
+    setShowAddDialog(!showAddDialog);
+  }, [showAddDialog]);
   return (
     <Table className="table-auto">
       <TableHead>
