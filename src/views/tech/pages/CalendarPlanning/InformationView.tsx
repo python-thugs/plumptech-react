a25c2efd31a59d6d5a5c50d0d5f190b5375ca598
plump_albert <plumpalbert@gmail.com>
2022-06-20 15:08:23 +0300

    added markup for side information

---
+++ src/views/tech/pages/CalendarPlanning/InformationView.tsx
@@ -0,0 +1,67 @@
+import Paper from "@mui/material/Paper";
+import T from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import {useMemo} from "react";
+// icons
+import CogIcon from "@mui/icons-material/Settings";
+import TimerIcon from "@mui/icons-material/Timer";
+// custom imports
+import {adapter} from "./MonthView";
+
+interface IProps {
+  date: Date;
+}
+
+const InformationView: React.FC<IProps> = ({date}) => {
+  const dateDisplay = useMemo(
+    () => (
+      <div className="flex flex-col text-center py-5 bg-blue-600 text-white">
+        <T component="h4" variant="h4" className="uppercase">
+          {adapter.format(date, "month")}
+        </T>
+        <T component="h1" variant="h1" className="font-semibold">
+          {adapter.format(date, "dayOfMonth")}
+        </T>
+        <T component="h5" variant="h5" className="font-light">
+          {adapter.format(date, "year")}
+        </T>
+      </div>
+    ),
+    [date]
+  );
+  return (
+    <Paper elevation={1} className="min-w-[300px]">
+      {dateDisplay}
+      <List className="py-3">
+        <ListItem className="justify-between">
+          <T variant="body1" className="font-medium">
+            Запланированные ТО
+          </T>
+          <ListItemIcon className="min-w-fit">
+            <CogIcon />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem>
+          <T variant="body1">Нет запланированных задач</T>
+        </ListItem>
+      </List>
+      <List className="py-3">
+        <ListItem className="justify-between">
+          <T variant="body1" className="font-medium">
+            Срок выполнения ТО
+          </T>
+          <ListItemIcon className="min-w-fit">
+            <TimerIcon />
+          </ListItemIcon>
+        </ListItem>
+        <ListItem>
+          <T variant="body1">Нет запланированных задач</T>
+        </ListItem>
+      </List>
+    </Paper>
+  );
+};
+
+export default InformationView;
--- src/views/tech/pages/CalendarPlanning/index.tsx
+++ src/views/tech/pages/CalendarPlanning/index.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import T from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 // icons
@@ -6,9 +6,11 @@ import LeftIcon from "@mui/icons-material/ChevronLeft";
 import RightIcon from "@mui/icons-material/ChevronRight";
 // custom imports
 import MonthView, {adapter} from "./MonthView";
+import InformationView from "./InformationView";
 
 const CalendarPlanning = () => {
   const [currentMonth, setMonth] = useState(new Date());
+  const [selectedDate, setDate] = useState(new Date());
 
   const selectPreviousMonth = useCallback(() => {
     setMonth(adapter.addMonths(currentMonth, -1));
@@ -18,25 +20,39 @@ const CalendarPlanning = () => {
     setMonth(adapter.addMonths(currentMonth, 1));
   }, [currentMonth]);
 
-  return (
-    <div className="flex flex-col py-12 gap-3 bg-gray-100 h-full">
-      <div className="flex flex-row items-center justify-center gap-4">
-        <IconButton onClick={selectPreviousMonth}>
-          <LeftIcon />
-        </IconButton>
-        <T
-          variant="h5"
-          component="h5"
-          className="uppercase leading-normal min-w-[200px] text-center"
-        >
-          {adapter.format(currentMonth, "monthAndYear")}
-        </T>
-        <IconButton onClick={selectNextMonth}>
-          <RightIcon />
-        </IconButton>
-      </div>
+  const handleDateSelect = useCallback<
+    React.ComponentProps<typeof MonthView>["onDateChange"]
+  >(date => setDate(date), []);
 
-      <MonthView month={currentMonth} />
+  useEffect(() => {
+    setMonth(selectedDate);
+  }, [selectedDate]);
+
+  return (
+    <div className="flex flex-row h-full w-full">
+      <div className="flex flex-col py-12 gap-3 bg-gray-100 h-full flex-1">
+        <div className="flex flex-row items-center justify-center gap-4">
+          <IconButton onClick={selectPreviousMonth}>
+            <LeftIcon />
+          </IconButton>
+          <T
+            variant="h5"
+            component="h5"
+            className="uppercase leading-normal min-w-[200px] text-center"
+          >
+            {adapter.format(currentMonth, "monthAndYear")}
+          </T>
+          <IconButton onClick={selectNextMonth}>
+            <RightIcon />
+          </IconButton>
+        </div>
+        <MonthView
+          active={selectedDate}
+          month={currentMonth}
+          onDateChange={handleDateSelect}
+        />
+      </div>
+      <InformationView date={currentMonth} />
     </div>
   );
 };
