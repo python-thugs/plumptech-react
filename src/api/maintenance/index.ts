570190e0aedb2897f0cf9fe852d71af66fd2b1d5
plump_albert <plumpalbert@gmail.com>
2022-06-21 12:54:28 +0300

    added type for maintenance response

--- src/api/maintenance/index.ts
+++ src/api/maintenance/index.ts
@@ -1,6 +1,15 @@
 import {joinPath as rootJoin} from "..";
+import {WithId} from "../types";
 export {getMaintenances} from "./getMaintenances";
 
 export function joinPath(...paths: string[]) {
   return rootJoin("maintenance", ...paths);
 }
+
+export type ResponseMaintenance = WithId<{
+  auto: number;
+  deadline: string;
+  end: string;
+  start: string;
+  status: number;
+}>;
