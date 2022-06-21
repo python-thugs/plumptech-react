46702d233c46dadaf9baf6336b91140fdcbc5d87
plump_albert <plumpalbert@gmail.com>
2022-06-21 11:30:43 +0300

    added types

--- src/api/types.ts
+++ src/api/types.ts
@@ -17,3 +17,23 @@ export enum PostEnum {
 }
 
 export type WithPassword<T> = T & {password: string};
+export type WithId<T> = T & {id: number};
+
+export interface IMaintenance {
+  start: Date;
+  deadline: Date;
+  end: Date;
+  auto: number | IAuto;
+  status: number | IStatus;
+}
+
+export interface IAuto {
+  licensePlate: string;
+  garagePlate: string;
+  manufacturer: string;
+  model: string;
+}
+
+export interface IStatus {
+  name: string;
+}
