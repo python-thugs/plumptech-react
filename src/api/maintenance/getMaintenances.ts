e1950b15b229ff9f0c1b77f174e4f8ee75b20fe3
plump_albert <plumpalbert@gmail.com>
2022-06-21 12:54:43 +0300

    updated request to fetch maintenances

--- src/api/maintenance/getMaintenances.ts
+++ src/api/maintenance/getMaintenances.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
-import {joinPath} from ".";
+import {joinPath, ResponseMaintenance} from ".";
+import {IMaintenance} from "../types";
 
 export async function getMaintenances() {
-  const response = await axios.get(joinPath());
-  return response.data;
+  const response = await axios.get<ResponseMaintenance[]>(joinPath());
+  return response.data.map<IMaintenance>(m => ({
+    ...m,
+    start: new Date(m.start),
+    deadline: new Date(m.deadline),
+    end: new Date(m.end),
+  }));
 }
