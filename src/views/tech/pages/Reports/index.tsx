959f4c96db56385c99718d6263ac80aa9350eb31
plump_albert <plumpalbert@gmail.com>
2022-06-23 08:26:37 +0300

    selecting maintenances from list

--- src/views/tech/pages/Reports/index.tsx
+++ src/views/tech/pages/Reports/index.tsx
@@ -13,13 +13,14 @@ import MuiListItemButton from "@mui/material/ListItemButton";
 import Divider from "@mui/material/Divider";
 // custom imports
 import {getMaintenances} from "../../../../api/maintenance";
+import {MaintenanceWithId} from "../../../../api/types";
 
 const ListItem: React.FC<React.ComponentProps<typeof MuiListItemButton>> = ({
   className,
   ...props
 }) => (
   <MuiListItemButton
-    className={`px-6 py-3 ${className || ""}`}
+    className={`px-4 py-3 rounded ${className || ""}`}
     {...props}
   ></MuiListItemButton>
 );
@@ -27,21 +28,43 @@ const ListItem: React.FC<React.ComponentProps<typeof MuiListItemButton>> = ({
 const TODAY = new Date();
 
 const ReportsPage = () => {
+  //#region State
+
   const [from, setFrom] = useState<Date | null>(TODAY);
   const [to, setTo] = useState<Date | null>(null);
+  const [selectedMaintenance, selectMaintenance] =
+    useState<MaintenanceWithId>();
+
+  //#endregion
+
+  //#region Queries
+
+  const maintenancesQuery = useQuery("maintenances", getMaintenances);
+
+  //#endregion
+
+  //#region Handlers
 
   const handleFromDateChange = useCallback((d: Date | null) => setFrom(d), []);
   const handleToDateChange = useCallback((d: Date | null) => setTo(d), []);
 
-  const maintenancesQuery = useQuery("maintenances", getMaintenances);
+  const createHandleMaintenanceClick = useCallback(
+    (m: MaintenanceWithId) => () => {
+      if (m.id === selectedMaintenance?.id) selectMaintenance(undefined);
+      else selectMaintenance(m);
+    },
+    [selectedMaintenance]
+  );
+
+  //#endregion
 
   return (
     <main className="h-full w-full flex flex-row">
-      <div className="flex flex-col flex-[2] gap-6 pt-6 px-5">
-        <T variant="h4" component="h4" className="px-3">
+      <div className="flex flex-col flex-[2] gap-6 pt-6">
+        <T variant="h4" component="h4" className="px-8">
           Отчетный период
         </T>
-        <div className="flex flex-row gap-2 items-center pt-2">
+        <div className="flex flex-row gap-2 items-center pt-2 px-5">
           <LocalizationProvider dateAdapter={AdapterDateFns} adapterLocale={ru}>
             <DesktopDatePicker
               label="Начальная дата"
@@ -61,19 +84,23 @@ const ReportsPage = () => {
             />
           </LocalizationProvider>
         </div>
-        <div className="flex flex-col gap-6 px-3 pt-4">
-          <T variant="h4" component="h4">
+        <div className="flex flex-col gap-6 pt-4">
+          <T variant="h4" component="h4" className="px-8">
             Информация
           </T>
-          <p className="flex flex-row gap-6 items-baseline m-0">
+          <p className="flex flex-row gap-6 items-baseline m-0 px-8">
             <T variant="body1" className="font-medium">
               Количество завершенных ТО:
             </T>
             <T variant="body1">2</T>
           </p>
-          <List className="gap-1">
+          <List className="gap-1 px-4">
             {maintenancesQuery.data?.map((j, i) => (
-              <ListItem key={`maintenance-item-${i}`}>
+              <ListItem
+                key={`maintenance-item-${i}`}
+                selected={j.id === selectedMaintenance?.id}
+                onClick={createHandleMaintenanceClick(j)}
+              >
                 {j.auto.manufacturer} {j.auto.model}
               </ListItem>
             ))}
